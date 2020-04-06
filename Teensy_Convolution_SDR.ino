848bf353f77edf7d295e8dad6e024758c32aa633
Frank <FrankBoesing@users.noreply.github.com>
2020-04-06 13:34:30 +0200

    fix ApproxAtan2

--- Teensy_Convolution_SDR.ino
+++ Teensy_Convolution_SDR.ino
@@ -16582,58 +16582,58 @@ float ApproxAtan(float z)
     return (n1 + n2 * z * z) * z;
 }
 
-#define PI_2 (PI / 2)
+#define PI_2 PIH
 
 float ApproxAtan2(float y, float x)
 {
-    if (x != 0.0f)
+  if (x != 0.0f)
+  {
+    if (fabsf(x) > fabsf(y))
     {
-        if (fabsf(x) > fabsf(y))
-        {
-            const float z = y / x;
-            if (x > 0.0)
-            {
-                // atan2(y,x) = atan(y/x) if x > 0
-                return ApproxAtan(z);
-            }
-            else if (y >= 0.0)
-            {
-                // atan2(y,x) = atan(y/x) + PI if x < 0, y >= 0
-                return ApproxAtan(z) + PI;
-            }
-            else
-            {
-                // atan2(y,x) = atan(y/x) - PI if x < 0, y < 0
-                return ApproxAtan(z) - PI;
-            }
-        }
-        else // Use property atan(y/x) = PI/2 - atan(x/y) if |y/x| > 1.
-        {
-            const float z = x / y;
-            if (y > 0.0)
-            {
-                // atan2(y,x) = PI/2 - atan(x/y) if |y/x| > 1, y > 0
-                return -ApproxAtan(z) + PI_2;
-            }
-            else
-            {
-                // atan2(y,x) = -PI/2 - atan(x/y) if |y/x| > 1, y < 0
-                return -ApproxAtan(z) - PI_2;
-            }
-        }
+      const float z = y / x;
+      if (x > 0.0f)
+      {
+        // atan2(y,x) = atan(y/x) if x > 0
+        return ApproxAtan(z);
+      }
+      else if (y >= 0.0f)
+      {
+        // atan2(y,x) = atan(y/x) + PI if x < 0, y >= 0
+        return ApproxAtan(z) + PI;
+      }
+      else
+      {
+        // atan2(y,x) = atan(y/x) - PI if x < 0, y < 0
+        return ApproxAtan(z) - PI;
+      }
     }
-    else
+    else // Use property atan(y/x) = PI/2 - atan(x/y) if |y/x| > 1.
     {
-        if (y > 0.0f) // x = 0, y > 0
-        {
-            return PI_2;
-        }
-        else if (y < 0.0f) // x = 0, y < 0
-        {
-            return -PI_2;
-        }
+      const float z = x / y;
+      if (y > 0.0f)
+      {
+        // atan2(y,x) = PI/2 - atan(x/y) if |y/x| > 1, y > 0
+        return -ApproxAtan(z) + PI_2;
+      }
+      else
+      {
+        // atan2(y,x) = -PI/2 - atan(x/y) if |y/x| > 1, y < 0
+        return -ApproxAtan(z) - PI_2;
+      }
     }
-    return 0.0f; // x,y = 0. Could return NaN instead.
+  }
+  else
+  {
+    if (y > 0.0f) // x = 0, y > 0
+    {
+      return PI_2;
+    }
+    else if (y < 0.0f) // x = 0, y < 0
+    {
+      return -PI_2;
+    }
+  }
+  return 0.0f; // x,y = 0. Could return NaN instead.
 }
 
 void T4_rtc_set(unsigned long t)
